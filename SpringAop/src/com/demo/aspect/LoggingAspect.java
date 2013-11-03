9d280778271cf0dd3c17719d60fbc90417fac372
OlgaE <olga.efimova.aol@gmail.com>
2013-11-03 10:35:59 +0700

    Using pointcuts and wildcards.

--- SpringAop/src/com/demo/aspect/LoggingAspect.java
+++ SpringAop/src/com/demo/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.demo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
@@ -17,6 +18,61 @@ public class LoggingAspect {
 	// with zero or any number of arguments:
 	@Before("execution(public * get*(..))")
 	public void LoggingAdvice2(){
+		System.out.println("Advice run. Get method called.");
+	}
+	
+	// Wild cards, for every getter 
+	// with any number of arguments (at least one):
+	@Before("execution(public * get*(*))")
+	public void LoggingAdvice3(){
+		System.out.println("Advice run. Get method called.");
+	}
+	
+	//
+	// *****
+	// "execution(public * get*(*))" is a pointcut for LoggingAdvice method.
+	// To define a pointcut separetely to be used for several methods:
+	@Pointcut("execution(public * get*(*))")
+	public void allGetters(){}
+	// This dummi method holds a pointcut expression.
+	
+	// Now this dummi method can be used as a pointcut for many methods:
+	@Before("allGetters()")
+	public void LoggingAdvice4(){
+		System.out.println("Advice run. Get method called.");
+	}
+	
+	@Before("allGetters()")
+	public void LoggingAdvice5(){
+		System.out.println("Advice run. Get method called.");
+	}
+	// *****
+	
+	// A pointcut for all methods in a particular class:
+	// (any return point, public or anything else)
+	@Pointcut("* * com.demo.model.Circle.*(..)")
+	public void allCircleMethods(){}
+	
+	@Before("allCircleMethods()")
+	public void LoggingAdvice6(){
+		System.out.println("Advice run. Get method called.");
+	}
+	
+	// Another way to say that a pointcut should apply for any method in a class:
+	@Pointcut("within(com.demo.model.Circle)")
+	public void AllCircleMethods2(){}
+	
+	// To apply a pointcut to all classes and methods in a package:
+	@Pointcut("within(com.demo.model.*)")
+	public void AllPackageMethods(){}
+	
+	// To apply a pointcut to all classes and methods in a package and subpackages:
+	@Pointcut("within(com.demo.model..*)")
+	public void AllSubPackageMethods(){}
+	
+	// To apply several pointcuts for an advice (&&):
+	@Before("allCircleMethods() && AllPackageMethods()")
+	public void LoggingAdvice7(){
 		System.out.println("Advice run. Get method called.");
 	}
 }
